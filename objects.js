fe4b1de7f7d279aaa4fdcdee081b79228410397f
Jamie Lopez <jmelopez@outlook.com>
2020-09-09 19:35:09 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {
-  avenged sevenfold: 'a little piece of heaven'
-  five finger death punch: 'a little bit off'
-  egypt central: 'white rabbit'
+  avenged sevenfold: 'a little piece of heaven',
+  five finger death punch: 'a little bit off',
+  egypt central: 'white rabbit',
   panic at the disco: 'old fashioned'
 }
